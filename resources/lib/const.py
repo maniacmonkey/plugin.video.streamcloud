26eb90be446e092a59e5899a45d85e71648e9852
Rolf Richter <rolf.richter@mail.de>
2015-02-02 16:45:57 +0100

    change service url to production

--- resources/lib/const.py
+++ resources/lib/const.py
@@ -3,6 +3,6 @@ import xbmcplugin
 
 BASE_URL = sys.argv[0]
 ADDON_HANDLE = int(sys.argv[1])
-SERVICE_URL = "http://scheinweb.de/sc-virtual"
+SERVICE_URL = "http://scheinweb.de/sc"
 LANG = xbmcplugin.getSetting(ADDON_HANDLE, 'prefLanguage')
 LANG_CODES = {'1': "DE", '2': 'EN'}
