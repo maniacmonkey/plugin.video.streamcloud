186fc15f6797af79b945128ed19adea1927f4f2e
maniacmonkey <pgsheahan@gmail.com>
2016-04-27 23:45:25 +0100

    Changed DE to ES

--- resources/lib/const.py
+++ resources/lib/const.py
@@ -5,4 +5,4 @@ BASE_URL = sys.argv[0]
 ADDON_HANDLE = int(sys.argv[1])
 SERVICE_URL = "http://scheinweb.de/sc"
 LANG = xbmcplugin.getSetting(ADDON_HANDLE, 'prefLanguage')
-LANG_CODES = {'1': "DE", '2': 'EN', '3': 'ES'}
+LANG_CODES = {'1': "ES", '2': 'EN'}
